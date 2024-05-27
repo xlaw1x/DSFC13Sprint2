3d7676c2329189e193335683be9f0ee08d2fb98f
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-27 16:33:45 +0800

    Update 02_Methodology.py

--- pages/02_Methodology.py
+++ pages/02_Methodology.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# import numpy as np
 import streamlit as st
 
 st.title("Hello world!")
