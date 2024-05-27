6daa278ab63ec67dd4a83611b9f5b7e16b854758
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-27 17:03:03 +0800

    Update 02_Methodology.py

--- pages/02_Methodology.py
+++ pages/02_Methodology.py
@@ -7,19 +7,18 @@ st.title("Hello world!")
 
 df = pd.read_csv('data/cc_rfm.csv')
 st.dataframe(df)
-st.write(df)
 
-# # Add some matplotlib code !
-# fig, ax = plt.subplots()
-# df.hist(
-#   bins=8,
-#   column="Age",
-#   grid=False,
-#   figsize=(8, 8),
-#   color="#86bf91",
-#   zorder=2,
-#   rwidth=0.9,
-#   ax=ax,
-# )
-# st.write(fig)
+# Add some matplotlib code !
+fig, ax = plt.subplots()
+df.hist(
+  bins=8,
+  column="Age",
+  grid=False,
+  figsize=(8, 8),
+  color="#86bf91",
+  zorder=2,
+  rwidth=0.9,
+  ax=ax,
+)
+st.write(fig)
 
