dca6ca2d76835062fa4ff0b1400824f6a99c107a
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-27 16:25:21 +0800

    Update 02_Methodology.py

--- pages/02_Methodology.py
+++ pages/02_Methodology.py
@@ -4,21 +4,21 @@ import streamlit as st
 
 st.title("Hello world!")
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
+# uploaded_file = st.file_uploader("Choose a file")
+# if uploaded_file is not None:
 df = pd.read_csv(Data/cc_rfm.csv)
 st.write(df)
 
-  # Add some matplotlib code !
-  fig, ax = plt.subplots()
-  df.hist(
-    bins=8,
-    column="Age",
-    grid=False,
-    figsize=(8, 8),
-    color="#86bf91",
-    zorder=2,
-    rwidth=0.9,
-    ax=ax,
-  )
-  st.write(fig)
+# Add some matplotlib code !
+fig, ax = plt.subplots()
+df.hist(
+  bins=8,
+  column="Age",
+  grid=False,
+  figsize=(8, 8),
+  color="#86bf91",
+  zorder=2,
+  rwidth=0.9,
+  ax=ax,
+)
+st.write(fig)
