eab073be31811f4b3be6e758456833e93a15cc0b
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-27 16:29:10 +0800

    Update 02_Methodology.py

--- pages/02_Methodology.py
+++ pages/02_Methodology.py
@@ -1,13 +1,11 @@
-# import matplotlib.pyplot as plt
-# import pandas as pd
-# import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
 
-# st.title("Hello world!")
+st.title("Hello world!")
 
-# # uploaded_file = st.file_uploader("Choose a file")
-# # if uploaded_file is not None:
-# df = pd.read_csv(Data/cc_rfm.csv)
-# st.write(df)
+df = pd.read_csv(Data/cc_rfm.csv)
+st.write(df)
 
 # # Add some matplotlib code !
 # fig, ax = plt.subplots()
@@ -23,28 +21,3 @@
 # )
 # st.write(fig)
 
-
-import matplotlib.pyplot as plt
-import pandas as pd
-import streamlit as st
-
-st.title("Hello world!")
-
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
-  st.write(df)
-
-  # Add some matplotlib code !
-  fig, ax = plt.subplots()
-  df.hist(
-    bins=8,
-    column="Age",
-    grid=False,
-    figsize=(8, 8),
-    color="#86bf91",
-    zorder=2,
-    rwidth=0.9,
-    ax=ax,
-  )
-  st.write(fig)
