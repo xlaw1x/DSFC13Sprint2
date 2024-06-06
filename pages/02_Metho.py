995d29e0f51a387b7c3e2e8c7ddde709476e4851
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-06 17:15:12 +0800

    Update 02_Metho.py

--- pages/02_Metho.py
+++ pages/02_Metho.py
@@ -1,7 +1,7 @@
-import matplotlib as plt
+import streamlit as st
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import streamlit as st
 import seaborn as sn
 # from wordcloud import WordCloud
 # from nltk.corpus import stopwords
