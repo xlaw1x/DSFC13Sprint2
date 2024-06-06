73597db9ee92b33fc3e234edffb789a839489434
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-07 00:39:20 +0800

    Update 02_Data_Cleaning_and_Outlier_Detection.py

--- pages/02_Data_Cleaning_and_Outlier_Detection.py
+++ pages/02_Data_Cleaning_and_Outlier_Detection.py
@@ -19,17 +19,22 @@ def main():
     numerical_cols = ['Latitude', 'Longitude']
     self_accident_quant = df[numerical_cols]
 
+    # Set Seaborn style and color palette
+    sns.set_style("whitegrid")
+    sns.set_palette(["#7ABAFF", "#1D2371"])
+
+
     # Plotting the distributions
     fig, ax = plt.subplots(1, 2, figsize=(16, 4))
     fig.suptitle('Distribution', fontsize=16)
 
     # Distribution of Latitude
-    sns.histplot(self_accident_quant["Latitude"], ax=ax[0], color="#7ABAFF")  # Light blue histogram
-    sns.kdeplot(self_accident_quant["Latitude"], ax=ax[0], color="#1D2371", linewidth=2)  # Dark blue KDE curve
+    sns.histplot(self_accident_quant["Latitude"], ax=ax[0], kde=True)  # Light blue histogram
+    #sns.kdeplot(self_accident_quant["Latitude"], ax=ax[0], color="#1D2371", linewidth=2)  # Dark blue KDE curve
     ax[0].set_title('Distribution of Latitude')
 
     # Distribution of Longitude
-    sns.histplot(self_accident_quant["Longitude"], ax=ax[1], color="#7ABAFF")  # Light blue histogram
+    sns.histplot(self_accident_quant["Longitude"], ax=ax[1])  # Light blue histogram
     #sns.kdeplot(self_accident_quant["Longitude"], ax=ax[1], color="#1D2371", linewidth=2)  # Dark blue KDE curve
     ax[1].set_title('Distribution of Longitude')
 
