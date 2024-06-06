16f401d76d0fcb0b6a681cc80cd66cbca9663ee1
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-07 00:10:14 +0800

    Update 02_Data_Cleaning_and_Outlier_Detection.py

--- pages/02_Data_Cleaning_and_Outlier_Detection.py
+++ pages/02_Data_Cleaning_and_Outlier_Detection.py
@@ -2,3 +2,35 @@ import streamlit as st
 
 st.title("Data Cleaning, Preprocessing, and Outlier Detection")
 st.write("Here you can put each of your key results.")
+
+
+# Streamlit app
+def main():
+    st.title('Distribution of Latitude and Longitude')
+    st.write('This app visualizes the distribution of latitude and longitude.')
+
+    # Display the DataFrame
+    st.write('Data:')
+    st.write(df)
+
+    # Extract numerical columns
+    numerical_cols = ['Latitude', 'Longitude']
+    self_accident_quant = df[numerical_cols]
+
+    # Plotting the distributions
+    fig, ax = plt.subplots(1, 2, figsize=(16, 4))
+    fig.suptitle('Distribution', fontsize=16)
+
+    # Distribution of Latitude
+    sns.histplot(self_accident_quant["Latitude"], ax=ax[0], color="#1D2371")  # Dark blue
+    ax[0].set_title('Distribution of Latitude')
+
+    # Distribution of Longitude
+    sns.histplot(self_accident_quant["Longitude"], ax=ax[1], color="#7ABAFF")  # Light blue
+    ax[1].set_title('Distribution of Longitude')
+
+    # Show the plot
+    st.pyplot(fig)
+
+if __name__ == '__main__':
+    main()
