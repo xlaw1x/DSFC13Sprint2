a7e349c1ca32c034697283e29ddd1eb2ff9854e1
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-07 00:28:53 +0800

    Update 02_Data_Cleaning_and_Outlier_Detection.py

--- pages/02_Data_Cleaning_and_Outlier_Detection.py
+++ pages/02_Data_Cleaning_and_Outlier_Detection.py
@@ -24,15 +24,12 @@ def main():
     fig.suptitle('Distribution', fontsize=16)
 
     # Distribution of Latitude
-    sns.histplot(self_accident_quant["Latitude"], ax=ax[0], color="#7ABAFF", kde=True,)  # Light blue with dark curve
+    sns.histplot(self_accident_quant["Latitude"], ax=ax[0], color="#7ABAFF", kde=True, line_color="#1D2371")  # Light blue with dark curve
     ax[0].set_title('Distribution of Latitude')
 
     # Distribution of Longitude
-    sns.histplot(self_accident_quant["Longitude"], ax=ax[1], color="#7ABAFF", kde=True)  # Light blue with dark curve
+    sns.histplot(self_accident_quant["Longitude"], ax=ax[1], color="#7ABAFF", kde=True, line_color="#1D2371")  # Light blue with dark curve
     ax[1].set_title('Distribution of Longitude')
 
     # Show the plot
     st.pyplot(fig)
-
-if __name__ == '__main__':
-    main()
