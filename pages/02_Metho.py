b91907c74f7157d152b48c6d18a5d6416c6f8999
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-06 17:21:44 +0800

    Update 02_Metho.py

--- pages/02_Metho.py
+++ pages/02_Metho.py
@@ -9,22 +9,51 @@ import seaborn as sn
 
 st.title("Hello world YAY!")
 
-df = pd.read_csv('data/cc_rfm.csv')
+df = pd.read_csv('data/involved_data_final.csv')
 #st.dataframe(df)
 
-# Add some matplotlib code !
-fig, ax = plt.subplots()
-df.hist(
-  bins=8,
-  column="Type",
-  grid=False,
-  figsize=(8, 8),
-  color="#86bf91",
-  zorder=2,
-  rwidth=0.9,
-  ax=ax,
-)
-st.write(fig)
+fraud_map = {0: 'multiple_accident', 1: 'self_accident'}
+numerical_df_eda['Class'] = numerical_df_eda['is_self_accident'].map(fraud_map)
+
+# Plotting function
+def plot_graph(data):
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sns.countplot(x=data['Class'],
+                  order=data['Class'].value_counts().index,
+                  color='skyblue')
+    plt.xlabel(' ')
+    plt.ylabel(' ')
+    for spine in ['right', 'top']:
+        ax.spines[spine].set_visible(False)
+    plt.title('Distribution of Vehicular Accidents in Metro Manila', size=15, y=1)
+    st.pyplot(fig)
+
+# Streamlit app
+def main():
+    st.title('Vehicular Accidents in Metro Manila')
+    st.write('This app visualizes the distribution of vehicular accidents.')
+
+    # Display the DataFrame
+    st.write('Data:')
+    st.write(numerical_df_eda['Class'].value_counts())
+
+    # Plot the graph
+    st.write('Graph:')
+    plot_graph(numerical_df_eda)
+
+# # Add some matplotlib code !
+# fig, ax = plt.subplots()
+# df.hist(
+#   bins=8,
+#   column="Type",
+#   grid=False,
+#   figsize=(8, 8),
+#   color="#86bf91",
+#   zorder=2,
+#   rwidth=0.9,
+#   ax=ax,
+# )
+# st.write(fig)
 
 # numerical_df=df
 # #fig, ax = plt.subplots()
