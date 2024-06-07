780385b2d94d7aecf56c6e0cefd23674c2e6c089
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-08 03:27:44 +0800

    Update 05_Model_Interpretability_and_Results.py

--- pages/05_Model_Interpretability_and_Results.py
+++ pages/05_Model_Interpretability_and_Results.py
@@ -4,3 +4,19 @@ st.title("Summary")
 st.write("Here you can provide a short summary and recommendations")
 
 st.image("images/shap_feature_importance.png", width=550)
+st.image("images/beeswarm.png")
+
+st.image("images/FP1.png")
+st.caption("Force Plot for Datapoint 1 That Is a self_accident with One Involved Vehicle")
+st.image("images/lIME1.png")
+st.caption("LIME Prediction Probabilities for a Datapoint 1 That Is a self_accident with One Involved Vehicle")
+
+st.image("images/FP2.png")
+st.caption("Force Plot for Datapoint 2 That Is a non_self_accident with One Involved Vehicle")
+st.image("images/lIME2.png")
+st.caption("LIME Prediction Probabilities for a Datapoint 2 That Is a non_self_accident with One Involved Vehicle")
+
+st.image("images/FP3.png")
+st.caption("Force Plot for Datapoint 3 That Is a non_self_accident with Multiple Involved Vehicles")
+st.image("images/lIME3.png")
+st.caption("LIME Prediction Probabilities for a Datapoint 3 That Is a non_self_accident with Multiple Involved Vehicles")
