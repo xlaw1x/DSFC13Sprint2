e506f2d99ed84c4a1946dcb1205c6e2d0b4c1d3b
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-07 21:08:40 +0800

    Update Project_Home.py

--- Project_Home.py
+++ Project_Home.py
@@ -1,8 +1,39 @@
 import streamlit as st
 
-def header(url):
-     st.markdown(f'<p style="background-color:#F5F0E8;color:#1B1954;font-size:24px;border-radius:2%;">{url}</p>', unsafe_allow_html=True)
-     
+# def header(url):
+#      st.markdown(f'<p style="background-color:#F5F0E8;color:#1B1954;font-size:24px;border-radius:2%;">{url}</p>', unsafe_allow_html=True)
+
+# Set page configuration
+st.set_page_config(
+    page_title="Your Page Title",
+    page_icon="Your Page Icon URL",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+# Set background color
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #f0f2f6; /* Change this to your desired background color */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Set text color
+st.markdown(
+    """
+    <style>
+    body, label, .css-vfskoc, .st-ab, .st-ag {
+        color: #333333; /* Change this to your desired text color */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 st.image('images/title.png')
 st.caption('Group 1 Exp1orers - Francesca Panga | Jacob Fuentebella | Jersy Carino | Leonard Inciso | Lester Covarrubias II')
 
