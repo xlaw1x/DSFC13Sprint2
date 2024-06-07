5780fcce761802f96452119b9e13327d19b00b10
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-07 21:26:33 +0800

    Update Project_Home.py

--- Project_Home.py
+++ Project_Home.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.image('images/title.png')
 st.caption('Group 1 Exp1orers - Francesca Panga | Jacob Fuentebella | Jersy Carino | Leonard Inciso | Lester Covarrubias II')
 
-st.title ('Introduction')
-st.subheader('Intro description', divider='blue')
+st.title ('Introduction', divider='blue')
+st.subheader('Intro description')
 st.image('images/mmda_geopandas.png')
 st.write('---')
 
