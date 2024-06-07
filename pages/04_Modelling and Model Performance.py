3cd23a1a9069de7f002d0bde6db30d219d6ec074
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-08 02:25:01 +0800

    Update 04_Modelling and Model Performance.py

--- pages/04_Modelling and Model Performance.py
+++ pages/04_Modelling and Model Performance.py
@@ -3,4 +3,32 @@ import streamlit as st
 st.title("Modelling and Model Performance")
 st.write("Given the imbalanced dataset, the group 5 resampling methods to see if these would improve the model's performance.")
 
+st.header("Oversampling Techniques")
 st.image('images/baseline.png')
+
+st.header("Oversampling Techniques") 
+st.image('images/SMOTE.png')
+st.caption("No Hypertuning, with SMOTE Oversampling")
+st.image('images/ADASYN.png')
+st.caption("No Hypertuning, with ADASYN Oversampling")
+st.image('images/SMOTEN.png')
+st.caption("No Hypertuning, with SMOTEN Oversampling")
+st.image('images/oversampling_comparison.png')
+st.caption("Comparison of Oversampling Methods")
+st.image('images/bar_graph.png')
+st.caption("Comparison of Oversampling Methods")
+
+st.header("Undersampling Techniques") 
+st.image('images/EditedNearestNeighbors.png')
+st.caption("No Hypertuning, with EditedNearestNeighbors Undersampling")
+st.image('images/TomekLinks.png')
+st.caption("No Hypertuning, with TomekLinks Undersampling")
+st.image('images/undersampling_comparison.png')
+st.caption("Comparison of Undersampling Methods")
+st.image('images/bar_graph_undersampling.png')
+st.caption("Comparison of Undersampling Methods")
+
+st.image('images/final_comparison.png')
+st.caption("Comparison of All Models")
+
+
