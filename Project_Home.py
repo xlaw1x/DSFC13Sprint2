b465151a20e8bcd29c317622c6408e5de5a33097
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-07 21:24:01 +0800

    Update Project_Home.py

--- Project_Home.py
+++ Project_Home.py
@@ -1,47 +1,5 @@
 import streamlit as st
 
-# # Set page configuration
-# st.set_page_config(
-#     page_title="Your Page Title",
-#     page_icon="Your Page Icon URL",
-#     layout="wide",
-#     initial_sidebar_state="expanded",
-# )
-
-# Set page configuration with light theme
-st.set_page_config(
-    page_title="Your Page Title",
-    page_icon="Your Page Icon URL",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    theme="light"  # Set the theme to "light"
-)
-
-# # Set background color
-# st.markdown(
-#     """
-#     <style>
-#     body {
-#         background-color: #30D5C8; /* Change this to your desired background color */
-#     }
-#     </style>
-#     """,
-#     unsafe_allow_html=True,
-# )
-
-# # Set text color
-# st.markdown(
-#     """
-#     <style>
-#     body, label, .css-vfskoc, .st-ab, .st-ag {
-#         color: #0000FF; /* Change this to your desired text color */
-#     }
-#     </style>
-#     """,
-#     unsafe_allow_html=True,
-# )
-
-
 st.image('images/title.png')
 st.caption('Group 1 Exp1orers - Francesca Panga | Jacob Fuentebella | Jersy Carino | Leonard Inciso | Lester Covarrubias II')
 
