a03e5f355f7363058ec6c8db861200194cb6f609
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-08 04:41:27 +0800

    Update Project_Home.py

--- Project_Home.py
+++ Project_Home.py
@@ -65,31 +65,31 @@ st.write(f'Prediction: {prediction}')
 # X_holdout = pd.read_csv('/content/drive/MyDrive/Eskwelabs/Notebooks/Filled Notebooks/holdout.csv', index_col=0)
 # holdout_accidents = X_holdout.index.to_list()
 
-st.title("Self-accident Detection")
-html_temp = """
-<div style="background:#025246 ;padding:10px">
-<h2 style="color:white;text-align:center;"> Self-accident Detection ML App </h2>
-</div>
-"""
-st.markdown(html_temp, unsafe_allow_html = True)
+# st.title("Self-accident Detection")
+# html_temp = """
+# <div style="background:#025246 ;padding:10px">
+# <h2 style="color:white;text-align:center;"> Self-accident Detection ML App </h2>
+# </div>
+# """
+# st.markdown(html_temp, unsafe_allow_html = True)
 
-#adding a selectbox
-choice = st.selectbox(
-    "Select Accident Number:",
-    options = holdout_accidents)
+# #adding a selectbox
+# choice = st.selectbox(
+#     "Select Accident Number:",
+#     options = holdout_accidents)
 
 
-def predict_is_self_accident(index):
-    accident = X_holdout.loc[index].values.reshape(1, -1)
-    prediction_num = model.predict(accident)[0]
-    pred_map = {1: 'is_self_accident', 0: 'is_not_self_accident'}
-    prediction = pred_map[prediction_num]
-    return prediction
+# def predict_is_self_accident(index):
+#     accident = X_holdout.loc[index].values.reshape(1, -1)
+#     prediction_num = model.predict(accident)[0]
+#     pred_map = {1: 'is_self_accident', 0: 'is_not_self_accident'}
+#     prediction = pred_map[prediction_num]
+#     return prediction
 
-if st.button("Predict"):
-    output = predict_is_self_accident(choice)
+# if st.button("Predict"):
+#     output = predict_is_self_accident(choice)
 
-    if output == 'is_self_accident':
-        st.error('This accident may be a self-accident', icon="🚨")
-    elif output == 'is_not_self_accident':
-        st.success('This is not a self-accident!', icon="✅")
+#     if output == 'is_self_accident':
+#         st.error('This accident may be a self-accident', icon="🚨")
+#     elif output == 'is_not_self_accident':
+#         st.success('This is not a self-accident!', icon="✅")
