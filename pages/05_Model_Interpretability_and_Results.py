053fe3ca98fa3738b0a9b00a4003f4666d571b4a
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-08 03:21:20 +0800

    Update 05_Model_Interpretability_and_Results.py

--- pages/05_Model_Interpretability_and_Results.py
+++ pages/05_Model_Interpretability_and_Results.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Summary")
 st.write("Here you can provide a short summary and recommendations")
 
-st.image("images/shap_feature_importance.png", width = 300, height=200)
+st.image("images/shap_feature_importance.png", height=200)
