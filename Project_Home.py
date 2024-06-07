8a89f1d0154124a32886053452b5aeb80d26a326
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-08 04:56:06 +0800

    Update Project_Home.py

--- Project_Home.py
+++ Project_Home.py
@@ -27,6 +27,7 @@ st.write('---')
 # import streamlit as st
 # import pickle
 
+(os.getcwd())
 
 
 import streamlit as st
@@ -36,40 +37,40 @@ import pandas as pd
 # # Load the model
 # model = pickle.load(open('data/baseline_model.pkl', 'rb'))
 # Load the model
-with open('baseline_model.pkl', 'rb') as f:
-    model = pickle.load(f)
+# with open('baseline_model.pkl', 'rb') as f:
+#     model = pickle.load(f)
 
-# Load the holdout data
-X_holdout = pd.read_csv('data/holdout.csv', index_col=0)
-holdout_accidents = X_holdout.index.to_list()
+# # Load the holdout data
+# X_holdout = pd.read_csv('data/holdout.csv', index_col=0)
+# holdout_accidents = X_holdout.index.to_list()
 
-# Streamlit app
-st.title("Self-accident Detection")
+# # Streamlit app
+# st.title("Self-accident Detection")
 
-html_temp = """
-<div style="background:#025246 ;padding:10px">
-<h2 style="color:white;text-align:center;"> Self-accident Detection ML App </h2>
-</div>
-"""
-st.markdown(html_temp, unsafe_allow_html=True)
+# html_temp = """
+# <div style="background:#025246 ;padding:10px">
+# <h2 style="color:white;text-align:center;"> Self-accident Detection ML App </h2>
+# </div>
+# """
+# st.markdown(html_temp, unsafe_allow_html=True)
 
-# Adding a selectbox
-choice = st.selectbox("Select Accident Number:", options=holdout_accidents)
+# # Adding a selectbox
+# choice = st.selectbox("Select Accident Number:", options=holdout_accidents)
 
-def predict_is_self_accident(index):
-    accident = X_holdout.loc[index].values.reshape(1, -1)
-    prediction_num = model.predict(accident)[0]
-    pred_map = {1: 'is_self_accident', 0: 'is_not_self_accident'}
-    prediction = pred_map[prediction_num]
-    return prediction
+# def predict_is_self_accident(index):
+#     accident = X_holdout.loc[index].values.reshape(1, -1)
+#     prediction_num = model.predict(accident)[0]
+#     pred_map = {1: 'is_self_accident', 0: 'is_not_self_accident'}
+#     prediction = pred_map[prediction_num]
+#     return prediction
 
-if st.button("Predict"):
-    output = predict_is_self_accident(choice)
+# if st.button("Predict"):
+#     output = predict_is_self_accident(choice)
 
-    if output == 'is_self_accident':
-        st.error('This accident may be a self-accident', icon="🚨")
-    elif output == 'is_not_self_accident':
-        st.success('This is not a self-accident!', icon="✅")
+#     if output == 'is_self_accident':
+#         st.error('This accident may be a self-accident', icon="🚨")
+#     elif output == 'is_not_self_accident':
+#         st.success('This is not a self-accident!', icon="✅")
 
 
 # # Streamlit app
