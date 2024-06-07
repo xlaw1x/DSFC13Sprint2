facf258433c455d59f0d89a386951935d3df6b26
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-08 01:47:14 +0800

    Update 04_Modelling and Model Performance.py

--- pages/04_Modelling and Model Performance.py
+++ pages/04_Modelling and Model Performance.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
-st.title("Summary")
-st.write("Here you can provide a short summary and recommendations")
+st.title("Modelling and Model Performance")
+st.write("Given the imbalanced dataset, the group 5 resampling methods to see if these would improve the model's performance.")
+
+st.title("Data Retrieval, Cleaning, and Preprocessing")
+st.write("The dataset used in this project was obtained from the Kaggle website found at: https://www.kaggle.com/datasets/esparko/mmda-traffic-incident-data.")
+st.write("This data from Kaggle was webscraped from the Metropolitan Manila Development Authority's (MMDA) official Twitter.") 
+
+st.write("The initial dataset has 17,312 rows and 13 columns as shown below.")
+st.image('images/dfinfo1.png')
