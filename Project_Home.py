d6ec23481d42802b5cf075d742a67f9448cd01b5
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-08 05:13:18 +0800

    Update Project_Home.py

--- Project_Home.py
+++ Project_Home.py
@@ -24,12 +24,11 @@ st.header('Methodology', divider='blue')
 st.image('images/methodology.png')
 st.write('---')
 
-import streamlit as st
 import pickle
 
-# Load the model
-with open('baseline_model.pkl', 'rb') as f:
-    model = pickle.load(f)
+# # Load the model
+# with open('baseline_model.pkl', 'rb') as f:
+#     model = pickle.load(f)
 
 # # Load the model
 # model = pickle.load(open('data/baseline_model.pkl', 'rb'))
