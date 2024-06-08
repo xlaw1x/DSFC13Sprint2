0503cd138dffda75e83214919cac077387808e46
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-08 10:31:07 +0800

    Update Project_Home.py

--- Project_Home.py
+++ Project_Home.py
@@ -20,7 +20,7 @@ st.header('Scope', divider='blue')
 st.markdown(
     """
     <div style="text-align: justify">
-        Our research focuses on road accidents from 2018 to 2020 within Metro Manila given its significant number of vehicular accidents. We aim to create a binary classification Machine Learning model that determines if a specific accident is a self-accident or not a self-accident. Self-accidents are single-vehicle accidents typically caused by road hazards (potholes, uneven road surface, misaligned barriers, poorly maintained infrastructure), speeding, distracted/reckless driving, or poor weather conditions. These are often underreported due to its nature being a minor incident. We want to detect these accidents to provide local government units (LGUs) with actionable insights to improve road safety. The goal is to find the model that optimizes precision score, as it is our aim to detect the most true positives as possible. 
+        Our research focuses on road accidents from 2018 to 2020 within Metro Manila given its significant number of vehicular accidents. We aim to create a binary classification Machine Learning model that determines if a specific accident is a self-accident or not a self-accident. Self-accidents are single-vehicle accidents typically caused by road hazards (potholes, uneven road surface, misaligned barriers, poorly maintained infrastructure), speeding, distracted/reckless driving, or poor weather conditions. These are often underreported due to its nature being a minor incident. We want to detect these accidents to provide local government units (LGUs) with actionable insights to improve road safety. The goal is to find the model that optimizes precision score, as it is our aim to detect the most true positives as possible.\n\n
     </div>
     """,
     unsafe_allow_html=True
@@ -29,13 +29,12 @@ st.markdown(
 st.markdown(
     """
     <div style="text-align: justify">
-        \nThe map below categorizes as gray points all vehicular accidents, while red points illustrate self-accidents (plotted such that areas with denser self-eccident occurences show as larger points).
+        The map below categorizes as gray points all vehicular accidents, while red points illustrate self-accidents (plotted such that areas with denser self-eccident occurences show as larger points).
     </div>
     """,
     unsafe_allow_html=True
 )
 
-st.write(').')
 st.image('images/mmda_geopandas.png')
 st.write('---')
 
