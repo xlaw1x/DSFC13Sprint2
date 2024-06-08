1ffef62e38f835f9798fa7e323ca48cfbaeb323b
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-08 09:14:00 +0800

    Update 06_streamlit_app.py

--- pages/06_streamlit_app.py
+++ pages/06_streamlit_app.py
@@ -1,3 +1,3 @@
-st.title("Would you like to try predicting a self-accident?"
-st.header("Head on over to the following link: https://social-gifts-sink.loca.lt/!)
-st.header("Don't forget to key in the Tunnel Code to enter: 34.67.93.189!)
+st.title("Would you like to try predicting a self-accident?")
+st.header("Head on over to the following link: https://social-gifts-sink.loca.lt/!")
+st.header("Don't forget to key in the Tunnel Code to enter: 34.67.93.189!")
