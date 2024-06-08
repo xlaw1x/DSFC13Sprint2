94c45ba97bd203b640eece85db2c74d250129f47
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-08 09:34:17 +0800

    Update 06_streamlit_app.py

--- pages/06_streamlit_app.py
+++ pages/06_streamlit_app.py
@@ -3,17 +3,8 @@ import streamlit as st
 st.title("Would you like to try predicting a self-accident?")
 #st.header("Head on over to the following link: https://social-gifts-sink.loca.lt/!")
 
-# # Display the local image
-# st.image("images/ml_app.png", caption="Image Caption", use_column_width=True)
-
-# # Display the local image as a clickable link button
-# st.markdown("[![Self-accident Detection ML App](st.image("images/ml_app.png")](https://social-gifts-sink.loca.lt/)")
-
-
-import streamlit as st
-
 # Display the local image
-#st.image("path_to_local_image.png", caption="Image Caption", use_column_width=True)
+st.image("images/ml_app.png", caption="Image Caption", use_column_width=True)
 
 # Display the local image as a clickable link button
-st.markdown("[![Button Text](st.image("images/ml_app.png"))](https://social-gifts-sink.loca.lt/)")
+st.markdown("[![Self-accident Detection ML App]("images/ml_app.png")](https://social-gifts-sink.loca.lt/)")
