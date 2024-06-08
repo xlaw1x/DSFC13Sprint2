b4f4825885491c83e7b73c60abd128154f117a37
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-08 11:39:22 +0800

    Update 04_Modelling and Model Performance.py

--- pages/04_Modelling and Model Performance.py
+++ pages/04_Modelling and Model Performance.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Modelling and Model Performance")
-st.write("Given the imbalanced dataset, the group tested 5 resampling methods (SMOTE, SMOTEN, ADASYN, TomekLinks, EditedNearestNeighbors) and 5 Machine Learning models (LogisticRegressor, GaussianNB, RandomForestClassifier, DecisionTreeClassifier, GradientBoostingClassifier) to see which combination would give the highest precision score. After all iterations were concluded, the group discovered that the best model was the baseline (GradientBoosting, no hypertuning, no resampling) with a train precision of 84.47%, validation precision of 76.12%, and holdout precision of 80.85%.')
+st.write("Given the imbalanced dataset, the group tested 5 resampling methods (SMOTE, SMOTEN, ADASYN, TomekLinks, EditedNearestNeighbors) and 5 Machine Learning models (LogisticRegressor, GaussianNB, RandomForestClassifier, DecisionTreeClassifier, GradientBoostingClassifier) to see which combination would give the highest precision score. After all iterations were concluded, the group discovered that the best model was the baseline (GradientBoosting, no hypertuning, no resampling) with a train precision of 84.47%, validation precision of 76.12%, and holdout precision of 80.85%.")
 
 st.header("Oversampling Techniques")
 st.image('images/baseline.png')
