d66132ffa2d45a105b98633617a9ab9aec71cdc6
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-08 09:22:10 +0800

    Update 06_streamlit_app.py

--- pages/06_streamlit_app.py
+++ pages/06_streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
 st.title("Would you like to try predicting a self-accident?")
-st.header("Head on over to the following link: https://social-gifts-sink.loca.lt/!")
+#st.header("Head on over to the following link: https://social-gifts-sink.loca.lt/!")
+
+# Display the image as a clickable link button
+st.markdown("[![ML APP](images/ml_app.png)](https://social-gifts-sink.loca.lt/)")
 st.header("Don't forget to key in the Tunnel Code to enter: 34.67.93.189!")
