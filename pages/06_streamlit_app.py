02c4850d802f6d0476c0e3376bd5f4f1833fe45a
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-08 09:38:30 +0800

    Update 06_streamlit_app.py

--- pages/06_streamlit_app.py
+++ pages/06_streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Display the local image
-st.image("images/ml_app.png", caption="Image Caption", use_column_width=True)
+st.image("images/ml_app.png", use_column_width=True)
 
 st.title("Would you like to try predicting a self-accident?")
 st.header("Head on over to the link below and don't forget to key in the Tunnel Code to enter: 34.67.93.189")
