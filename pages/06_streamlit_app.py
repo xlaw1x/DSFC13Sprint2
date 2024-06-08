319aacf835501309f65019fd7563a4139f6239ea
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-06-08 09:37:51 +0800

    Update 06_streamlit_app.py

--- pages/06_streamlit_app.py
+++ pages/06_streamlit_app.py
@@ -7,4 +7,4 @@ st.title("Would you like to try predicting a self-accident?")
 st.header("Head on over to the link below and don't forget to key in the Tunnel Code to enter: 34.67.93.189")
 
 # Display a clickable link with custom text
-st.markdown("[Self-accident Detection ML App](https://social-gifts-sink.loca.lt/))")
+st.markdown("[Self-accident Detection ML App](https://social-gifts-sink.loca.lt/)")
